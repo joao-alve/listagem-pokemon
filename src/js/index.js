033e487f041655cd16b8bf3f938318ba4bb82ca4
jã1 <133174148+joao-alve@users.noreply.github.com>
2023-05-14 10:58:37 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const botaoAlterarTema = document.getElementById("botão-altera-tema");
+const body = document.querySelector("body");
+botaoAlterarTema.addEventListener("click", ()=>{
+body.classList.add("modo.escuro");
+
+});
+
